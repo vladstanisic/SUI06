3b3978c2ea31617b0dbcc9aff372519bcbc956e8
Quzymac <33623813+Quzymac@users.noreply.github.com>
2017-11-13 14:04:37 +0100

    vik

---
+++ NewSchoolStuff/Assets/Scripts/Bullet.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    private Transform target;
+
+    [SerializeField] float speed = 0.5f;
+   
+    [SerializeField] string enemytag = "Enemy";
+
+    //[SerializeField] Transform gameObject;
+    [SerializeField] float turnSpeed = 10;
+    
+
+
+    // Use this for initialization
+    void Start()
+    {
+       
+    }
+    
+
+    public void ChaseTarget(Transform targetTransform)
+    {
+        target = targetTransform;
+    }
+	
+	
+	// Update is called once per frame
+	void Update () {
+        if (target == null)
+        {
+            Destroy(base.gameObject);
+            return;
+        }
+
+        Vector3 dir = target.position - transform.position;
+        float distanceThisFrame = speed * Time.deltaTime;
+
+        if (dir.magnitude <= distanceThisFrame)
+        {
+            HitTarget();
+            return;
+        }
+
+        transform.Translate(dir.normalized * distanceThisFrame, Space.World);
+
+        Quaternion lookRotation = Quaternion.LookRotation(dir);
+        /*Vector3 rotation = lookRotation.eulerAngles;
+        gameObject.transform.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+        */
+        gameObject.transform.rotation = lookRotation;
+    }
+    void HitTarget()
+    {
+        Destroy(gameObject);
+        // damage enemy here
+    }
+}
---
+++ NewSchoolStuff/Assets/Scripts/Canon.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Canon : MonoBehaviour {
+
+    [SerializeField] Transform target;
+    [SerializeField] float range = 5f;
+    [SerializeField] float fireRate = 1f;
+    private float fireCountdown = 0f;
+
+    [SerializeField] string enemytag = "Enemy";
+
+    [SerializeField] Transform partToRotate;
+    [SerializeField] float turnSpeed = 10;
+
+    [SerializeField] GameObject bulletPrefab;
+    [SerializeField] Transform firePoint;
+    
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);// kallar updateTarget 2 gånger/ sek
+	}
+
+    void UpdateTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemytag);
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+
+        foreach(GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if(distanceToEnemy < shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+
+        if (nearestEnemy != null && shortestDistance <= range)
+        {
+            target = nearestEnemy.transform;
+        }
+        else
+        {
+            target = null;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		if(target == null)
+        {
+            return;
+        }
+
+        Vector3 dir = target.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(dir);
+        Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles; // lerp smoothens the initial rotation on lock
+        
+        partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+
+        if (fireCountdown <= 0f)
+        {
+            Shoot();
+            fireCountdown = 1f / fireRate;
+        }
+
+        fireCountdown -= Time.deltaTime;
+        
+	}
+
+    void Shoot()
+    {
+        
+        GameObject bulletGO = (GameObject)Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Bullet bullet = bulletGO.GetComponent<Bullet>();
+
+        if(bullet != null)
+        {
+            bullet.ChaseTarget(target);
+        }
+    }
+
+    private void OnDrawGizmosSelected() //ritar ut canon range 
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+}
